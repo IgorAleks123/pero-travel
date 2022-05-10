19a19298bf1091fe004143ee5ae018bd1bf3dff4
IgorAleks123 <98655162+IgorAleks123@users.noreply.github.com>
2022-05-10 12:21:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+	const menuBody = document.querySelector('.header__menu');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
+}
+
