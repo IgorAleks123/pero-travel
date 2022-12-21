5451fd5e9224199cc8b6044b182b7e11f1aad367
IgorAleks123 <98655162+IgorAleks123@users.noreply.github.com>
2022-12-21 22:19:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,41 @@ if (iconMenu) {
 		document.body.classList.toggle('_lock');
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
-	})
-}
+	});
+};
+
+const priceIcon = document.querySelector('.menu__plus');
+if (priceIcon) {
+	const priceBody = document.querySelector('.filter__price');
+	priceIcon.addEventListener("click", function(e) {
+		priceIcon.classList.toggle('_active_price');
+		priceBody.classList.toggle('_active_price');
+	});
+};
+
+const priceIconOne = document.querySelector('.menu__plus_1');
+if (priceIconOne) {
+	const priceBodyOne = document.querySelector('.filter__people-hide');
+	priceIconOne.addEventListener("click", function(e) {
+		priceIconOne.classList.toggle('_active_price');
+		priceBodyOne.classList.toggle('_active_price');
+	});
+};
+
+const priceIconTwo = document.querySelector('.menu__plus_2');
+if (priceIconTwo) {
+	const priceBodyTwo = document.querySelector('.filter__place-radio');
+	priceIconTwo.addEventListener("click", function(e) {
+		priceIconTwo.classList.toggle('_active_price');
+		priceBodyTwo.classList.toggle('_active_price');
+	});
+};
+
+
+
+
+
+
+
+
 
